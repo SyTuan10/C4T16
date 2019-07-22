34139cdbffdb01d0a0549b2d247cd4aaca9d8b03
SyTuan <SyTuan10@gmail.com>
2019-07-23 00:43:06 +0700

    Continued

--- Web-Session1/test.js
+++
@@ -1,9 +0,0 @@
-var deleteBtns = document.getElementsByClassName('delete_btn')
-for(var i=0; i<deleteBtns.length; i++){
-    var deleteBtn = deleteBtns[i];
-    deleteBtn.addEventListener('click', function(e){
-        var btn = e.target;
-        var div = btn.parentNode;
-        div.remove();
-    })
-}
